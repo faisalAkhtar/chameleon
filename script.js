2a4884fb4e1c990700ecd780020f5f0a1682c8dc
Faisal Vostro - Ubuntu <faisalakhtar1999@gmail.com>
2020-10-08 00:04:25 +0530

    Code not needed anymore

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
 let x = Math.floor(Math.random() * 5)
 console.log(x)
 document.head.lastElementChild.setAttribute("href", "stylesheets/style" + x + ".css");
-
-document.querySelector(".menuHead").onclick = function () {
-    document.querySelector("nav").classList.toggle("opened")
-}
-
-document.querySelector("nav a").onclick = function () {
-    this.parentElement.classList.toggle("opened")
-}
